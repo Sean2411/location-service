bde98af8e83399676f074b884d0095a871e855e2
Sean Lu <Sean.Lu@jetblue.com>
2020-04-12 16:50:42 -0400

    created index.js

---
+++ locationService/index.js
@@ -0,0 +1,9 @@
+'use strict'
+const AWS = require('aws-sdk');
+
+exports.handler = function(event, context, callback) {
+  console.log(JSON.stringify(`Event: event`))
+  // Lambda Code Here
+  // context.succeed('Success!')
+  // context.fail('Failed!')
+}
