c254ab82c431c24b15665b8b17141296b1542be4
Sean Lu <Sean.Lu@jetblue.com>
2020-04-15 20:27:45 -0400

    inti firebase

--- locationService/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const AWS = require('aws-sdk');
-
-exports.handler = function(event, context, callback) {
-  console.log(JSON.stringify(`Event: event`))
-  // Lambda Code Here
-  // context.succeed('Success!')
-  // context.fail('Failed!')
-}
